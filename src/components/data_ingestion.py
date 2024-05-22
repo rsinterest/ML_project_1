abdb04503dd973d552e2233e3b04ce48d4919b9d
2023-SQL-HeLili <2725731392@qq.com>
2024-05-22 21:23:05 +0800

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,1 +1,49 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd 
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('artifacts', 'train.csv')
+    test_data_path: str=os.path.join('artifacts', 'test.csv')
+    raw_data_path: str=os.path.join('artifacts', 'data.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_congif=DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Enter the data ingestion method or component")
+        try:
+            df=pd.read_csv('notebook\data\stud.csv')
+            logging.info('Read the dateset as dataframe')
+
+            os.makedirs(os.path.dirname(self.ingestion_congif.train_data_path), exist_ok=True)
+
+            df.to_csv(self.ingestion_congif.raw_data_path, index=False, header=True)
+
+            logging.info("Train test split initiated")
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+            train_set.to_csv(self.ingestion_congif.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_congif.test_data_path, index=False, header=True)
+
+            logging.info("Ingestion of the data is completed")
+            return(
+                self.ingestion_congif.train_data_path,
+                self.ingestion_congif.test_data_path
+            )
+        except Exception as e:
+            return CustomException(e, sys)
+
+if __name__ == "__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
+
+
 
